e902a3195445804a7694e5d8e04f3b6c3c2df06e
Carla Daniela Nascimento <carla.danielaju@gmail.com>
2024-11-22 15:40:50 -0300

    commit inicial

---
+++ src/pages/Inicio/Inicio.jsx
@@ -0,0 +1,11 @@
+
+import s from "./inicio.module.scss"
+
+export default function Inicio() {
+    return (
+      <section className={s.banner}>
+        <h2>VENHA FAZER PARTE DA MAIOR REDE DE DOAÇÃO</h2>
+      </section>
+    );
+  }
+  
